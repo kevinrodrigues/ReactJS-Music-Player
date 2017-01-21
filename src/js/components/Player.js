8853ab276a26049e71490adadb8f9c998eb3a426
kevinrodrigues <Kevin@kevins-MBP.home>
2017-01-21 14:28:58 +0000

    Added react music app.

--- src/js/components/Player.js
+++ src/js/components/Player.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Album from "./Album";
 import TrackDetails from "./TrackDetails";
-import Scrubber from "./Scrubber";
+import ProgressBar from "./ProgressBar";
 import Controls from "./Controls";
 import TimeStamp from "./TimeStamp";
 import Audio from "./audio/Audio";
@@ -18,13 +18,39 @@ export default class Player extends React.Component {
         };
     }
 
+    defaultProps() {
+        return {
+            "name": "Say you won't let go",
+            "artist": "James Authur",
+            "album": "Back from the Edge",
+            "year": "2016",
+            "albumCover": "../img/album.jpg",
+            "duration": 210,
+            "source": "../assets/demo.mp3"
+        }
+    }
+
     togglePlaying() {
         let playingStatus = this.state.isPlaying;
         let el = document.getElementById('audio');
 
         if (playingStatus) {
+            let that = this;
+
             playingStatus = false;
             el.play();
+
+            setInterval(function() {
+                let time = el.currentTime;
+                let trackTime = that.defaultProps().duration;
+                let percent = (time / trackTime) * 100 + '%';
+
+                that.updateProgressBar(percent);
+                that.updatePlayingTime(time);
+
+
+            }, 100);
+
         } else {
             playingStatus = true;
             el.pause();
@@ -33,29 +59,30 @@ export default class Player extends React.Component {
         this.setState({isPlaying: playingStatus});
     }
 
-    render() {
-        let trackDetails = {
-            "name": "Say you won't let go",
-            "artist": "James Authur",
-            "album": "Back from the Edge",
-            "year": "2016",
-            "albumCover": "../img/album.jpg",
-            "duration": 210,
-            "source": "../assets/demo.mp3"
-        };
+    updateProgressBar(percent) {
+        let el = document.querySelector('.progress');
 
+        el.style.width = percent;
+    }
+
+    updatePlayingTime(time) {
+        time = Math.floor(time);
+        this.setState({currentTime: time});
+    }
+
+    render() {
         return (
             <div className="player-container">
-                <Album albumCover={trackDetails.albumCover}/>
+                <Album albumCover={this.defaultProps().albumCover}/>
                 <div className="player">
                     <div className="track-info">
-                        <TrackDetails name={trackDetails.name} artist={trackDetails.artist} album={trackDetails.album}/>
-                        <Scrubber/>
+                        <TrackDetails name={this.defaultProps().name} artist={this.defaultProps().artist} album={this.defaultProps().album}/>
                         <Controls isPlaying={this.state.isPlaying} onClick={this.togglePlaying.bind(this)}/>
-                        <TimeStamp duration={trackDetails.duration} currentTime={this.state.currentTime}/>
+                        <TimeStamp duration={this.defaultProps().duration} currentTime={this.state.currentTime}/>
                     </div>
-                    <Audio src={trackDetails.source}/>
+                    <Audio src={this.defaultProps().source}/>
                 </div>
+                <ProgressBar/>
             </div>
 
         );
---
+++ src/js/components/ProgressBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ProgressBar extends React.Component {
+    render() {
+        return (
+            <div className="progress-bar">
+                <div class="progress"></div>
+            </div>
+        );
+    }
+}
